1edea87b8416477d10395fc1cdb1eb4fc8a3b7d5
hannahsvaughan <hannahvaughan421@gmail.com>
2023-03-05 07:23:29 -0500

    added basic navbar and header

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Hannah Vaughan</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About Me</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+  
+  export default NavBar;
